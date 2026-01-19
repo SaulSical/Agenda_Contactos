0290d16b4ff09c1170ebdc1cb6d6c6501e89e780
Saul Sical <ssical-2024422@kinal.edu.gt>
2026-01-19 16:01:51 -0600

    Login Proyecto

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const container = document.querySelector(".container");
+const btnSingIn = document.querySelector("#btn-sing-in");
+const btnSingUp = document.querySelector("#btn-sing-up");
+
+btnSingIn.addEventListener("click", () => {
+    container.classList.remove("toggle");
+});
+
+btnSingUp.addEventListener("click", () => {
+    container.classList.add("toggle");
+});
